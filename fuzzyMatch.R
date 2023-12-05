53438cfa079edf6b47294c33a7dfd9fdeef0d4cf
Andrew Park <awpark@uga.edu>
2023-12-05 17:12:53 -0500

    improved fuzzyMatch analysis

--- fuzzyMatch.R
+++ fuzzyMatch.R
@@ -9,7 +9,6 @@ for (i in 1:57){
 }
 
 
-library(ape)
 newTree <- ape::read.tree("12862_2017_958_MOESM2_ESM.tre")
 
 for (i in 1:length(newTree$tip.label)){
@@ -20,14 +19,8 @@ newTree$tip.label[i] <- paste(strsplit(newTree$tip.label[i],split="_")[[1]][1],
 
 setdiff(f$Parasite_species,newTree$tip.label)
 
-library(fishtree)
-f %<>% dplyr::filter(grepl("sp\\.",Host_species)==F)
-f %<>% dplyr::filter(grepl("sp\\.",Parasite_species)==F)
 
-tr <- fishtree::fishtree_phylogeny(species=f$Host_species)
-missF <- setdiff(f$Host_species,tr$tip.label)
-missF <- gsub("_"," ",missF)
-library(rfishbase)
+
 tmp <- rfishbase::synonyms(missF)
 tmp %<>% mutate(alt=0)
 
@@ -38,14 +31,31 @@ for (i in 1:dim(tmp)[1]){
 tmp %<>% dplyr::select(synonym,Species)
 tmp %<>% dplyr::mutate(manual="TBD")
 
+##### MAYBE JUST NEED FROM HERE
+
+library(tidyverse)
+library(magrittr)
+library(ape)
+library(fishtree)
+library(rfishbase)
 library(fuzzyjoin)
+
+
+source(knitr::purl("paraPrep.qmd",quiet=T))
+
+
+f %<>% dplyr::filter(grepl("sp\\.",Host_species)==F)
+f %<>% dplyr::filter(grepl("sp\\.",Parasite_species)==F)
+
 tr <- fishtree::fishtree_phylogeny()
-df1 <- as_tibble(missF)
+missF <- setdiff(f$Host_species,tr$tip.label)
+
+fz <- as_tibble(missF)
 df2 <- as_tibble(tr$tip.label)
-df1 %<>% stringdist_join(.,df2, 
+fz %<>% stringdist_join(.,df2, 
                 by='value', #match based on team
                 mode='left', #use left join
-                method = "jw", #use jw distance metric
+                method = "jw",p=0, #use jw distance metric, p=0 means jaro vs winkler
                 max_dist=99, 
                 distance_col='dist')%>%
                 group_by(value.x) %>%
